8bb15fd6c76170f533b9c57d076e4b6437d52f0a
roger chahoud <roger_chahoud@outlook.com>
2023-07-27 21:16:05 -0300

    middleware for validation of authentication

--- src/controller/incident.controller.js
+++ src/controller/incident.controller.js
@@ -4,14 +4,9 @@ const create = async (req, res) => {
 	try {
 		const { number, title, description } = req.body;
 		if (!number || !title || !description) return res.status(400).send({ message: "Submit all fields" });
-		
-		const incident = await incidentService.createService({
-			number,
-			title,
-			description,
-			user: {_id: "64c2a1e7895da89ec28dcafc"}
-		});
 
+		//req.userId vem de um middlaware, que só funcionará se o usuário tiver um token valido 
+		const incident = await incidentService.createService({ number, title, description, user: req.userId }); // { _id: req.userId }
 		if (!incident) return res.status(400).send({ message: "Error creating incident" });
 
 		return res.status(201).send({ message: "Incident has been created successfully" });
@@ -57,12 +52,12 @@ const findById = async (req, res) => {
 
 const update = async (req, res) => {
 	try {
-		if (!req.body.number && !req.body.title && !req.body.description && !req.body.createdAt && !req.body.status) 
+		if (!req.body.number && !req.body.title && !req.body.description && !req.body.createdAt && !req.body.status)
 			return res.status(400).send({ message: "Submit at least one field" });
 
 		const updatedIncident = await incidentService.updateService(req.params.id, req.body);
 		if (!updatedIncident) return res.status(400).send({ message: "Error in update the incident" });
-		
+
 		return res.status(200).send({ message: "Incident updated successfully" });
 
 	} catch (err) {
--- src/route/incident.route.js
+++ src/route/incident.route.js
@@ -1,9 +1,10 @@
 import incidentController from "../controller/incident.controller.js";
 import { Router } from 'express';
+import {authMiddleware} from '../middleware/auth.middleware.js';
 
 const router = Router();
 
-router.post('/', incidentController.create);
+router.post('/', authMiddleware, incidentController.create);
 router.get('/', incidentController.findAll);
 router.get('/:id', incidentController.findById);
 router.patch('/:id', incidentController.update);
